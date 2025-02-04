fe4647f60632e3d228714f4154dce40764d75094
tirimia <11174371+tirimia@users.noreply.github.com>
2025-02-04 02:40:24 +0000

    [create-pull-request] automated change

--- bob.nix
+++ bob.nix
@@ -1,13 +1,13 @@
 {pkgs, ...}:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "bob";
-  version = "4.0.2";
+  version = "4.0.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "MordechaiHadad";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-IyMd8D/FwsIA4H9nWsq7vVp/9lmEn+m+aVEtajLwy/8=";
+    hash = "sha256-ftj9+A7Sf83dqtbu6xT53qaQd1DPkVvDhcOoiSXJKYo=";
   };
 
   cargoLock = {
